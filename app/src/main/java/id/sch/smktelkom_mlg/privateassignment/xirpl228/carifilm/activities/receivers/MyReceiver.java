f324f1434556733ac00090df5d8fa3fa94b40964
taufan99 <atalarikt@gmail.com>
2017-05-15 18:44:27 +0700

    MyReceiver added

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/carifilm/activities/receivers/MyReceiver.java
@@ -0,0 +1,35 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl228.carifilm.activities.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by Mokleters on 5/15/2017.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (isNetworkAvailable(context)) {
+            Intent i = new Intent("INTERNET_LOST");
+            i.putExtra("status", true);
+            context.sendBroadcast(i);
+        } else {
+            Intent i = new Intent("INTERNET_LOST");
+            i.putExtra("status", false);
+            context.sendBroadcast(i);
+        }
+    }
+
+    private boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+}
