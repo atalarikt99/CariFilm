f5b6e4e0a61250919b20824033fa4a517b1163bd
taufan99 <atalarikt@gmail.com>
2017-05-16 16:49:29 +0700

    Package moved

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/carifilm/fragments/MainFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl228/carifilm/fragments/MainFragment.java
@@ -1,4 +1,4 @@
-package id.sch.smktelkom_mlg.privateassignment.xirpl228.carifilm;
+package id.sch.smktelkom_mlg.privateassignment.xirpl228.carifilm.fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.privateassignment.xirpl228.carifilm.R;
 
 
 /**
